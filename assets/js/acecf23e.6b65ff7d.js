4302b5ad99147b2b95143905865767d8538e0c65
tigarto <helpu1409@gmail.com>
2024-06-27 21:34:35 -0500

    Deploy website - based on 737ef232a97d85c1c4e26da7d119260c37deca5a

---
+++ assets/js/acecf23e.6b65ff7d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkudea_so=self.webpackChunkudea_so||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/udea-so/blog","blogTitle":"Blog"}')}}]);
--- assets/js/c15d9823.ffc6c996.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkudea_so=self.webpackChunkudea_so||[]).push([[8146],{9328:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
